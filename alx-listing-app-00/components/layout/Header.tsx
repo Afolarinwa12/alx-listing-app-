8cc6a481963630a6b4c69f2364304d922b20a73b
Afolarinwa12 <Afolarinwa12@gmail.com>
2025-06-01 20:26:48 +0100

    task 5

---
+++ alx-listing-app-00/components/layout/Header.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Link from 'next/link';
+
+const Header: React.FC = ({ children }) => {
+  return (
+        <div>
+            <link>
+            <a></a>
+            </link>
+        </div>
+
+        /**hhhh */
+   
+  );
+}
+export default Header;
